536fd3ac3356b8aaef839b39e257e08e6b965989
Felipe André <fass@icomp.ufam.edu.br>
2021-08-22 23:02:58 -0400

    Incorporado código dos slides

--- senoideAGWN.m
+++ senoideAGWN.m
@@ -17,12 +17,29 @@ snrBoaDB   = 10 * log10(snrBoa  );
 snrComumDB = 10 * log10(snrComum);
 snrRuimDB  = 10 * log10(snrRuim );
 
-%% Obtendo AWGN do Matlab
-matlabAWGN = awgn(original, snrRuimDB, 'measured');
+snrDB = snrBoaDB;
 
-%% Plotando o seno no tempo
+%% Obtendo AWGN do Matlab
+%matlabAWGN = awgn(original, snrDB, 'measured');
+
+%% Importando código fornecido nos slides
+gamma = 10 ^ (snrDB / 10);
+
+% Cálculo da força do sinal
+p = sum(abs(original) .^ 2) / length(original);
+
+% Cálculo da densidade espectral do ruído
+n0 = (p / gamma);
+
+% Gerando o sinal de ruído
+ruido = sqrt(n0 / 2) * randn(size(original));
+
+% Por fim, o sinal recebido é a soma do sinal original com o ruído
+recebido = original + ruido;
+
+%% Plotando os sinais
 figure;
-plot(t,original,t,matlabAWGN);
+plot(t, original, t, recebido);
 xlabel('Tempo (s)');
 ylabel('Amplitude (V)');
 title('Sinal no tempo');
